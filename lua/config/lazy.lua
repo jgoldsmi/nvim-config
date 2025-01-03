8a9c06cbab9c7f7bb25bb7a07d688246ad1100c1
Jonathon Goldsmith <jonathon.goldsmith@gmail.com>
2025-01-03 08:49:43 -0500

    Add extras for elixir/erlang

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -26,6 +26,8 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.formatting.black" },
     -- { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.docker" },
+    { import = "lazyvim.plugins.extras.lang.elixir" },
+    { import = "lazyvim.plugins.extras.lang.erlang" },
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
