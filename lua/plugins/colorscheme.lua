5889cdaaba189d0d376d66f998c2701e86d2d72f
Jonathon Goldsmith <jonathon.goldsmith@gmail.com>
2026-01-23 10:50:48 -0500

    Remove catppuccin bufferline fix

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -14,13 +14,4 @@ return {
       },
     },
   },
-  {
-    "akinsho/bufferline.nvim",
-    init = function()
-      local bufline = require("catppuccin.groups.integrations.bufferline")
-      function bufline.get()
-        return bufline.get_theme()
-      end
-    end,
-  },
 }
